444c59e61c9b9afdaa1ed8922f5fb71d2cc21674
Vasile Popescu <elisescu@elisescu.com>
2022-10-21 16:16:24 +0200

    Add a github action to deploy to gh-pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
-import Layout from '../components/layout'
+import Layout from "../components/layout";
+
+const Circle = (props: { width: number; height: number }) => {
+  return (
+    <div
+      className="border-primary-900 block
+                  border-[20px] rounded-full
+                  hover:border-[30px]
+                  w-[200px] h-[200px]
+                  hover:w-[260px] hover:h-[260px]
+                  transition-all"
+      // style={{
+      //   width: props.width,
+      //   height: props.height,
+      //   "&:hover": { width: 1.2 * props.width, height: 1.2 * props.height },
+      // }}
+    ></div>
+  );
+};
 
 export default function Index() {
   return (
     <>
-      <Layout>
-          <div>  </div>
-      </Layout>
+      <div className="gap-1 h-screen grid grid-flow-col items-center justify-center">
+        {[...Array(1)].map((i) => (
+          <Circle width={200} height={200} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
