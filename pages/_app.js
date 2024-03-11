268bfeb79cae62fa6f0e20ce6800bcb360a16079
Vasile Popescu <elisescu@elisescu.com>
2024-03-11 11:08:10 +0100

    Add posthog integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/index.css'
+import posthog from 'posthog-js'
+
+if (typeof window !== 'undefined') {
+  posthog.init('phc_OisA5lvLboCSR3VzYhqmXqNNQf0A91yybVKXMJiHNu5', {
+    api_host: 'https://eu.posthog.com',
+  })
+}
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
